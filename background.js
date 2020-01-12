afd756eb61819967584991434f6ce3b479f039a1
Jasterix <ljcalixte@gmail.com>
2020-01-11 23:35:00 -0500

    send background message

--- background.js
+++ background.js
@@ -1,1 +1,12 @@
-console.log("Background running");
+console.log("Background running and listening for browser action");
+
+const buttonClicked = tab => {
+  let msg = {
+    txt: "hello"
+  };
+
+  console.log("hi");
+  chrome.tabs.sendMessage(tab.id, msg);
+};
+
+chrome.browserAction.onClicked.addListener(buttonClicked);
--- content.js
+++ content.js
@@ -5,3 +5,9 @@ let paragraphs = document.querySelectorAll("p");
 for (let el of paragraphs) {
   el.style["background-color"] = "#FF00FF";
 }
+
+const gotMessage = (message, sender, sendResponse) => {
+  console.log(message.txt);
+};
+
+chrome.runtime.onMessage.addListener(gotMessage);
